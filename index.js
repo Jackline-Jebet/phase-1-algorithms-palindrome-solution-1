e6c9d3ef0d1568e9033d6c330491819cc1358afc
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-17 15:35:53 +0300

    commit tree 701bc6bc382b4d9db0b1d494e00bde39cd6e90b9

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = reverseString(word);
+  return word === reverseWord
 }
 
 /* 
@@ -13,9 +19,21 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
+  console.log("EXpecting: true");
+  console.log("=>", isPalindrome("a"));
+  console.log("");
+
+  console.log("EXpecting: true");
+  console.log("=>", isPalindrome("abba"));
+  console.log("");
+
+  console.log("EXpecting: false");
+  console.log("=>", isPalindrome("dab"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"))
   console.log("");
 
   console.log("Expecting: false");
